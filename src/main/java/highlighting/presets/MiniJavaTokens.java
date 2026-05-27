f5a4b748c0f93285201e3d5f10e17ecf0cc5ec32
Nick Resner <117851151+NikitaTsundora@users.noreply.github.com>
2026-05-27 23:08:57 +0200

    Fix regex pattern for Java keywords in MiniJavaTokens

--- src/main/java/highlighting/presets/MiniJavaTokens.java
+++ src/main/java/highlighting/presets/MiniJavaTokens.java
@@ -28,12 +28,13 @@ public final class MiniJavaTokens {
         /* Keywords (als ganze Wörter, nicht als Teil anderer Bezeichner oder Kommentare o.ä.):
         package, import, class, public, private, final, return, null, new*/
         Token.of(
-            Pattern.compile("\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue"
-                            + "|default|do|double|else|enum|extends|final|finally|float|for|goto|if"
-                            + "|implements|import|instanceof|int|interface|long|native|new|package"
-                            + "|private|protected|public|return|short|static|strictfp|super|switch"
-                            + "|synchronized|this|throw|throws|transient|try|void|volatile|while"
-                            + "|true|false|null|var|record|sealed|yield)\\b"),
+            Pattern.compile(
+                "\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue"
+                    + "|default|do|double|else|enum|extends|final|finally|float|for|goto|if"
+                    + "|implements|import|instanceof|int|interface|long|native|new|package"
+                    + "|private|protected|public|return|short|static|strictfp|super|switch"
+                    + "|synchronized|this|throw|throws|transient|try|void|volatile|while"
+                    + "|true|false|null|var|record|sealed|yield)\\b"),
             MiniJavaColours.KEYWORD_COLOUR));
   }
 }
